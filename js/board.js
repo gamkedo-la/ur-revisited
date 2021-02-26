4837640ba92168b4d6621afc0c807384d656a955
RyanCahela <6194542+RyanCahela@users.noreply.github.com>
2021-02-26 13:56:36 -0800

    Adds Stick font and draws main title to canvas

--- js/board.js
+++ js/board.js
@@ -111,8 +111,21 @@ function drawBoard() {
     drawTileY += TILE_H;
     var drawTileX = BOARD_X_OFFSET;
     } // end of for each row
+  drawTitle();
 } // end of draw board 
 
 function drawPieces() {
   //
+}
+
+function drawMainTitle() {
+  var canvasWidth = TILE_W * BOARD_COLS;
+  canvasContext.save();
+  canvasContext.textBaseline = "top";
+  canvasContext.font = "20pt Stick";
+  canvasContext.fillStyle = "black";
+  canvasContext.textAlign = "center";
+  console.log(canvasWidth);
+  canvasContext.fillText("Teh Game of UR", canvas.width / 2, 15);
+  canvasContext.restore();
 }
